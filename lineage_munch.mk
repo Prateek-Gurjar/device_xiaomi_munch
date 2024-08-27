c47898fd5b85b68213ff256d62ce10ab7fb38387
DeadlyShroud <prateekgurjar786@protonmail.com>
2024-08-27 07:14:45 +0000

    munch: add flags for auto builds

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -14,6 +14,32 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
+# Flags
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon 870 5G" \
+    RISING_MAINTAINER="Prateek Gurjar"
+RISING_PACKAGE_TYPE := VANILLA_AOSP
+TARGET_ENABLE_BLUR := true
+WITH_GMS := false
+TARGET_HAS_UDFPS := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BUILD_PACKAGE := 1
+TARGET_LAUNCHER := 1
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_MATLOG := false
+ALPHA_BUILD_TYPE := Unofficial
+ALPHA_MAINTAINER := PrateekGurjar
+MATRIXX_BUILD_TYPE := Unofficial
+MATRIXX_MAINTAINER := PrateekGurjar
+MATRIXX_CHIPSET := SM8250
+MATRIXX_BATTERY := 4500mah
+MATRIXX_DISPLAY := 1080×2400
+EVEREST_MAINTAINER := PrateekGurjar
+WITH_GAPPS := false
+TARGET_SUPPORTS_BLUR := true
+
 PRODUCT_NAME := lineage_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
